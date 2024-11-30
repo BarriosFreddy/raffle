c93bd2fbabf59a5c0d932e5c36cb432df9121285
Freddy Barrios <freddybarrios.077@gmail.com>
2024-11-30 10:21:22 -0500

    Add copy file after build

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,24 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { viteStaticCopy } from 'vite-plugin-static-copy'
+import path from 'path'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: '.htaccess',
+          dest: './'
+        },
+      ],
+    }),],
   server: {
     host: true,
     port: 5173
   },
   resolve: {
-    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json'],
+    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.htaccess'],
     alias: {
       '@': '/src'
     }
