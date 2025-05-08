fd72333ea7b142b0cf6947ec5e0918ea0e318d52
Freddy Barrios <freddybarrios.077@gmail.com>
2025-05-08 16:15:50 -0500

    ADD style class

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ export default function App() {
   return (
     <ErrorBoundary>
       <Header />
-      <div className="h-auto bg-gray-50">
+      <div className="min-h-screen bg-gray-50">
         <main className="max-w-7xl mx-auto px-4 py-6">
           <h1 className="text-3xl font-bold text-gray-800 m-4">Dinámicas</h1>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
