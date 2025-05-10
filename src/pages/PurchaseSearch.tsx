24ef5939d48c3313621a1cdbda9d032dea90ab04
Freddy Barrios <freddybarrios.077@gmail.com>
2025-05-10 15:59:12 -0500

    FIX: ui changes

--- src/pages/PurchaseSearch.tsx
+++ src/pages/PurchaseSearch.tsx
@@ -43,7 +43,7 @@ export function PurchaseSearch() {
   };
 
   return (
-    <div className="bg-white rounded-xl shadow-lg p-6">
+    <div className="w-full max-w-5xl mx-auto bg-white rounded-xl shadow-lg p-6 mt-10">
       <h2 className="text-xl font-bold text-gray-900 mb-6">
         Encontrar mis números
       </h2>
